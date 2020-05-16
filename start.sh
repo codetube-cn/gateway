4a180af80c72b2e3d1aeb607544510893ecb691e
dai <daijulong@qq.com>
2020-05-17 03:48:37 +0800

    for docker compose

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 export MICRO_REGISTRY=etcd
 export MICRO_REGISTRY_ADDRESS=localhost:2379
 export MICRO_CLIENT=grpc
-export MICRO_API_NAMESPACE=codetube.cn.api
+export MICRO_NAMESPACE=cn.codetube
+export MICRO_API_NAMESPACE=cn.codetube
 micro api
