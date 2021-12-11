db9b747636f6f9094b000691223d3687ef83f631
daijulong <daijulong@qq.com>
2021-12-12 02:59:01 +0800

    调试首页HTML

--- bootstrap/start.go
+++ bootstrap/start.go
@@ -3,6 +3,7 @@ package bootstrap
 import (
 	"codetube.cn/gateway/config"
 	"codetube.cn/gateway/interfaces"
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -14,12 +15,16 @@ func Start() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		if route := config.Routes.Match(request); route != nil {
 			remote, _ := url.Parse(route.Url)
+			//fmt.Println(remote)
 			exchange := interfaces.BuildServerWebExchange(request)
 			responseFilters := route.FilterRequest(exchange)
 			proxy := httputil.NewSingleHostReverseProxy(remote)
 			proxy.ModifyResponse = func(response *http.Response) error {
 				responseFilters.Filter(response)
 				return nil
+			}
+			proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, err error) {
+				fmt.Println(err)
 			}
 			proxy.ServeHTTP(writer, request)
 		} else {
